d546a07dd66c5f0f1e92cdebd8d5fb2132108afd
Dozier Danielle <dozierdanielle@ymail.com>
2025-09-20 07:59:52 +0300

    Complete Movie Directory Lab with full routing, nested routes, and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-const App = () => {
-    return (
-        <>
-        {/* define all routes here */}
-        </>
-    )
-}
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import DirectorContainer from './components/DirectorContainer'
+import DirectorCard from './components/DirectorCard'
+import DirectorForm from './components/DirectorForm'
+import MovieForm from './components/MovieForm'
+import ErrorPage from './components/ErrorPage'
 
-export default App
+export default function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/directors" element={<DirectorContainer />}>
+          <Route path="new" element={<DirectorForm />} />
+          <Route path=":id" element={<DirectorCard />}>
+            <Route path="movies/new" element={<MovieForm />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
